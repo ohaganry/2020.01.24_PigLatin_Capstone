14c5b350c4263dc7bb7e912090ad00410fb6c002
ohaganry <hermits.nature@gmail.com>
2020-01-25 20:22:19 -0500

    saturday push, finished commenting ToPigLatin method & fixed some errors in word recognition

--- pigLatin/Program.cs
+++ pigLatin/Program.cs
@@ -8,9 +8,16 @@ namespace pigLatin
     {
         static void Main(string[] args)
         {
+            bool proceed = true;
+            while(proceed)
+            {
             string userInput = GetUserInput("Enter your phrase:").ToLower();
             string pigLatin = ToPigLatin(userInput);
             Console.WriteLine(pigLatin);
+            proceed = ValidateInput();
+            }
+            Console.WriteLine("ood-gay ye-bay");
+
         }
         public static string GetUserInput(string message)
         {
@@ -35,49 +42,47 @@ namespace pigLatin
         }
         public static string ToPigLatin(string input)
         {
+            //vowels variable used to run through string
             string vowels = "aeiou";
+            //create list of strings to join after translation
             List<string> newWords = new List<string>();
+            //foreach runs through each word in the input .Split breaks the sentence into individual words
             foreach(string word in input.Split())
             {
-                if(word.Length == 1)
+                //for single letter words add -way to the end since single letter words are a & I
+                if(word.Length == 1 || vowels.Contains(word[0]) && word.Length > 2)
                 {
-                    newWords.Add(word + "way");
+                    // adds converted word to List
+                    newWords.Add(word + "-ay");
                 }
-                if(word.Length == 2 && vowels.Contains(word[0]))
+                //for two letter words that start with a vowel (ex. am, I'm, an, in, etc.) or has no vowels adds -ay
+                if((word.Length == 2 && vowels.Contains(word[0])) || (word.Length == 2 && !vowels.Contains(word[0]) && !vowels.Contains(word[1])))
                 {
-                newWords.Add(word + "ay");
+                    //adds converted word to list
+                    newWords.Add(word + "ay");
                 }
-
+                //for two letter words that end with a vowel (ex. to, do, so, etc.)
                 if (word.Length == 2 && vowels.Contains(word[1]) && !vowels.Contains(word[0]))
                 {
-                    newWords.Add(word.Substring(1) + word.Substring(0, 1) + "ay");
+                    //takes index (1) and places it infornt of index (2) and ends with -ay and adds to list
+                    newWords.Add(word.Substring(1) + "-" + word.Substring(0, 1) + "ay");
                 }
-
-                if (word.Length == 2 && !vowels.Contains(word[1]) && !vowels.Contains(word[0]))
-                {
-                    newWords.Add(word + "ay");
-                }                
-
-                for (int i = 1; i < word.Length; i++)
-                {
-                    if (vowels.Contains(word[i]) && (vowels.Contains(word[0])))
-                    {
-                    newWords.Add(word.Substring(i) + word.Substring(0, i) + "ay");
-                    break;
-                    }
-                 }
-
                 for (int i = 0; i < word.Length; i++)
                 {
-                if (vowels.Contains(word[i]) && !(vowels.Contains(word[0])) && word.Length > 2)
-                {
-                    newWords.Add(word.Substring(i) + word.Substring(0, i) + "ay");
-                    break;
-                }
-
-                }
-                    
-                         
+                    //looks for index of first vowel & confirms it is not in index [0] & that the word is more than 2 letters long
+                    if (vowels.Contains(word[i]) && (!vowels.Contains(word[0])) && word.Length > 2)
+                    {
+                        //cuts the word from vowel index to end of string & adds on indexes before vowel to the end & adds ay to the end
+                        newWords.Add(word.Substring(i) + "-" + word.Substring(0, i) + "ay");
+                        break;
+                    }
+                    //for any words 
+                    //if(vowels.Contains(word[0]) && word.Length > 2)
+                    //{
+                        //newWords.Add(word + "way");
+                        //break;
+                    //}
+                }        
             }
             return string.Join(" ", newWords);
         }
